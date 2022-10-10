c3168d9806edf94a5601b80943791155d4bc9d3c
Lucien Le Roux <rshn.kabu@gmail.com>
2022-10-10 19:22:00 +0200

    Change baud rate to 56700 and write to serial port

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,13 @@ fn main () -> ! {
     let pins = arduino_hal::pins!(dp);
 
     let mut led = pins.d13.into_output();
+    let mut serial = arduino_hal::default_serial!(dp, pins, 56700);
 
     loop {
         led.set_high();
+        serial.write_byte(b'A');
         arduino_hal::delay_ms(100);
         led.set_low();
-        arduino_hal::delay_ms(1000);
+        arduino_hal::delay_ms(900);
     }
 }
